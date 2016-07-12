30467c17e95b7f9cbb8e0e1f2f019cfbf220cfe8
Patrick Ayres <briceayres@gmail.com>
2016-07-12 15:19:23 -0400

    starter project inital commit

---
+++ Gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var bs = require('browser-sync').create();
+
+// our browser-sync config + nodemon chain
+gulp.task('browser-sync', function() {
+    bs.init(null, {
+        proxy: "http://localhost:3000",
+        browser: "chromium-browser",
+        port: 4000,
+    });
+});
+
+// the real action
+gulp.task('default', ['browser-sync'], function () {
+    gulp.watch('./views/**/*.jade', bs.reload);
+    gulp.watch('./public/**/*.js', bs.reload);
+    gulp.watch('./public/**/*.sass', bs.reload);
+    gulp.watch('./routes/*.js', bs.reload);
+    gulp.watch('./*.js', bs.reload);
+    gulp.watch('./public/**/*.png', bs.reload);
+});
